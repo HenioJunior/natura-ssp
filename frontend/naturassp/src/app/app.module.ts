7a026cdd45588ab93d855e49bce6c68903b9445f
Henio Junior <henioajunior@gmail.com>
2022-05-29 16:11:13 -0300

    Criação da Navbar/Rodape

--- frontend/naturassp/src/app/app.module.ts
+++ frontend/naturassp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
