2385f03b64e1899042f77b11bcf2dc833f32f238
Henio Junior <henioajunior@gmail.com>
2022-05-30 16:15:56 -0300

    Downgrade Angular para 11

--- frontend/src/app/model/Produto.ts
+++ frontend/src/app/model/Produto.ts
@@ -1,11 +1,11 @@
 import { Categoria } from "./Categoria";
 
 export class Produto {
-    public id!: number;
-    public nome!: string;
-    public descricao!: string;
-    public linkFoto!: string;
-    public preco!: number;
-    public disponivel!: number;
-    public categoria!: Categoria;
+    public id: number;
+    public nome: string;
+    public descricao: string;
+    public linkFoto: string;
+    public preco: number;
+    public disponivel: number;
+    public categoria: Categoria;
 }
